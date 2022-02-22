a2b92ec9603ac114e26c633499bede091229a105
Archasion <james.nswork@gmail.com>
2022-02-22 12:34:34 +0000

    Added date construction and timestamp type selection

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
 			document.documentElement.style.setProperty("--secondary-box-color", "#F4F4F4");
 			document.documentElement.style.setProperty("--bg-color", "#E8E8E8");
 			document.documentElement.style.setProperty("--accent-color", "#5267fa");
+			document.documentElement.style.setProperty("--input-color", "#fff");
 		} else if ($("#theme-icon").attr("src").includes("dark-theme")) {
 			$("#theme-icon").attr("src", "icons/light-theme.svg");
 			document.documentElement.style.setProperty("--primary-text-color", "#fff");
@@ -17,6 +18,7 @@ $(document).ready(function () {
 			document.documentElement.style.setProperty("--secondary-box-color", "#25272c");
 			document.documentElement.style.setProperty("--bg-color", "#2f3136");
 			document.documentElement.style.setProperty("--accent-color", "#5267fa");
+			document.documentElement.style.setProperty("--input-color", "#464a52");
 		}
 	});
 });
