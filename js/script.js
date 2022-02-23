50ff296d30ababb4afe1240c675885a11fba60c3
Archasion <james.nswork@gmail.com>
2022-02-23 08:49:43 +0000

    Used function

--- js/script.js
+++ js/script.js
@@ -127,27 +127,33 @@ $(document).ready(function () {
 	$("#toggle-theme").click(function () {
 		if ($("#theme-icon").attr("src").includes("light-theme")) {
 			$("#theme-icon").attr("src", "icons/dark-theme.svg");
-			document.documentElement.style.setProperty("--primary-text-color", "#313131");
-			document.documentElement.style.setProperty("--secondary-text-color", "#00000050");
-			document.documentElement.style.setProperty("--primary-box-color", "#fff");
-			document.documentElement.style.setProperty("--secondary-box-color", "#F4F4F4");
-			document.documentElement.style.setProperty("--tertiary-box-color", "#00000021");
-			document.documentElement.style.setProperty("--bg-color", "#E8E8E8");
-			document.documentElement.style.setProperty("--pattern-color", "#dbdde4");
-			document.documentElement.style.setProperty("--accent-color", "#5267fa");
-			document.documentElement.style.setProperty("--input-color", "#fff");
+
+			setColor("primary-text-color", "#313131");
+			setColor("secondary-text-color", "#00000050");
+			setColor("primary-box-color", "#fff");
+			setColor("secondary-box-color", "#F4F4F4");
+			setColor("tertiary-box-color", "#00000021");
+			setColor("bg-color", "#E8E8E8");
+			setColor("pattern-color", "#dbdde4");
+			setColor("accent-color", "#5267fa");
+			setColor("input-color", "#fff");
 		} else if ($("#theme-icon").attr("src").includes("dark-theme")) {
 			$("#theme-icon").attr("src", "icons/light-theme.svg");
-			document.documentElement.style.setProperty("--primary-text-color", "#fff");
-			document.documentElement.style.setProperty("--secondary-text-color", "#dcddde");
-			document.documentElement.style.setProperty("--tertiary-text-color", "#ffffff6d");
-			document.documentElement.style.setProperty("--primary-box-color", "#18191b");
-			document.documentElement.style.setProperty("--secondary-box-color", "#25272c");
-			document.documentElement.style.setProperty("--tertiary-box-color", "#ffffff13");
-			document.documentElement.style.setProperty("--bg-color", "#2f3136");
-			document.documentElement.style.setProperty("--pattern-color", "#27282c");
-			document.documentElement.style.setProperty("--accent-color", "#5267fa");
-			document.documentElement.style.setProperty("--input-color", "#464a52");
+
+			setColor("primary-text-color", "#fff");
+			setColor("secondary-text-color", "#dcddde");
+			setColor("tertiary-text-color", "#ffffff6d");
+			setColor("primary-box-color", "#18191b");
+			setColor("secondary-box-color", "#25272c");
+			setColor("tertiary-box-color", "#ffffff13");
+			setColor("bg-color", "#2f3136");
+			setColor("pattern-color", "#27282c");
+			setColor("accent-color", "#5267fa");
+			setColor("input-color", "#464a52");
 		}
 	});
+
+	function setColor(variable, color) {
+		document.documentElement.style.setProperty(`--${variable}`, color);
+	}
 });
